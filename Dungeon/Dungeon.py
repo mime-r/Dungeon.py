bbc841eefc2418eb689f4bd1db7ab607d9c69f54
Samuel Cheng <samuelchengweien@gmail.com>
2021-07-30 08:12:21 +0800

    Update Dungeon.py

--- Dungeon/Dungeon.py
+++ Dungeon/Dungeon.py
@@ -1,6 +1,5 @@
 # Workflow
-
-
+from subprocess import call
 import os
 import Application.checkmodules as c
 dependencies = ["fuckit", "pandas", "tinydb", "termcolor", "keyboard"]
@@ -9,5 +8,4 @@ dependencies = ["fuckit", "pandas", "tinydb", "termcolor", "keyboard"]
 c.check(dependencies, "dungeon")
 os.system("cls")
 
-# only start the application after modules are checked.
-import Application.main as m
+call(["python", "Application/main.py"])
